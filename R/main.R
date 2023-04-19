6be6fd7b3d2db3a5be7e08e97ef7e0b01e5bbaea
Ariel Rokem <arokem@gmail.com>
2023-04-19 12:53:56 -0700

    Keep the subjectID as factor.

--- R/main.R
+++ R/main.R
@@ -171,7 +171,7 @@ tractr_single_bundle <- function(df_afq = NULL,
     df_afq[[group.by]] <- factor(df_afq[[group.by]])
   }
 
-  df_afq[[participant.id]] <- unclass(factor(df_afq[[participant.id]]))
+  df_afq[[participant.id]] <- factor(df_afq[[participant.id]])
 
   cols <- unique(c(participant.id,
                    "nodeID",
