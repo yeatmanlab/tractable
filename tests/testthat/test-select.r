31e53ad983bf074b54de55745f944f28360af086
Ariel Rokem <arokem@gmail.com>
2023-06-23 10:53:24 -0700

    Test for the case where "all" tracts are selected.

--- tests/testthat/test-select.r
+++ tests/testthat/test-select.r
@@ -16,4 +16,17 @@ tract_names <- selected$tract_names
 
 expect_identical(unique(df_tract$tractID), "Right Corticospinal")
 expect_identical(tract_names, c("Right Corticospinal"))
+
+ selected <- expect_no_error(
+    select_bundle(df_afq = sarica,
+                  tract = "all",
+                  dwi_metric = "fa",
+                  covariates = c("age", "group"),
+                  participant_id = "subjectID",
+                  group_by = "group")
+                )
+df_tract <- selected$df_tract
+tract_names <- selected$tract_names
+
+expect_identical(length(tract_names), as.integer(20))
     })
