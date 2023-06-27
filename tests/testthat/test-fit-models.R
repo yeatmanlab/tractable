3e58c68b1b88e44a6a3beaf2ac3d99703f22031a
Ariel Rokem <arokem@gmail.com>
2023-06-27 13:58:33 -0700

    Better setup for the tests. Also fixes failer.

--- tests/testthat/test-fit-models.R
+++ tests/testthat/test-fit-models.R
@@ -1,4 +1,3 @@
-test_that("fit_gam runs as expected", {
 
  sarica <- read.afq.sarica()
  sarica$group <- factor(sarica$class)
@@ -23,38 +22,36 @@ formula <- tractr::build_formula(
     participant_id = "subjectID",
     k = 40)
 
-# One and only one of "target" and "formula" should be set to non-NULL
-gam_fit <- expect_error(tractr::fit_gam(df_tract = df_tract,
-                                        target = NULL,
-                                        formula = NULL))
-
-gam_fit <- expect_error(tractr::fit_gam(df_tract = df_tract,
-                                        target = "fa",
-                                        formula = formula))
-
-gam_fit <- expect_no_error(tractr::fit_gam(df_tract = df_tract,
-                                           formula = formula))
-
-# Check that formula can be passed as a string
-string_formula = 'fa ~ age + group + s(nodeID, by = group, k = 40) + s(subjectID,
-    bs = "re")'
-gam_fit <- expect_no_error(tractr::fit_gam(df_tract = df_tract,
-                                           formula = string_formula))
-                                           })
-
 test_that("fit_gam runs as expected", {
+    # One and only one of "target" and "formula" should be set to non-NULL
+    gam_fit <- expect_error(tractr::fit_gam(df_tract = df_tract,
+                                            target = NULL,
+                                            formula = NULL))
+
+    gam_fit <- expect_error(tractr::fit_gam(df_tract = df_tract,
+                                            target = "fa",
+                                            formula = formula))
+
+    gam_fit <- expect_no_error(tractr::fit_gam(df_tract = df_tract,
+                                            formula = formula))
+
+    # Check that formula can be passed as a string
+    string_formula = 'fa ~ age + group + s(nodeID, by = group, k = 40) + s(subjectID,
+        bs = "re")'
+    gam_fit <- expect_no_error(tractr::fit_gam(df_tract = df_tract,
+                                            formula = string_formula))
+                                            })
+
+test_that("predicting with gams works as expected", {
+    gam_fit <- tractr::fit_gam(df_tract = df_tract,
+                            formula = formula)
 
 
-gam_fit <- tractr::fit_gam(df_tract = df_tract,
-                           target = "fa",
-                           formula = formula)
+    # Drop the FA column, because that's what we're predicting:
+    new_data <- df_tract[, !(names(df_tract) %in% c("fa"))]
 
-
-# Drop the FA column, because that's what we're predicting:
-new_data <- df_tract[, !(names(df_tract) %in% c("fa"))]
-
-expect_no_error(
-    new_data <- predict_with_gam(gam_fit, new_data, "fa")
-    )
-expect_equal(dim(new_data), dim(df_tract))
+    expect_no_error(
+        new_data <- predict_with_gam(gam_fit, new_data, "fa")
+        )
+    expect_equal(dim(new_data), dim(df_tract))
 })
